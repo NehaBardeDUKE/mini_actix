bee593f9c5d9b2a1f8c2d3b9dc99689c4f5de2ae
NehaBardeDUKE <110474064+NehaBardeDUKE@users.noreply.github.com>
2023-02-22 12:33:10 -0500

    update changes

---
+++ mockingjayactix/scr/lib.rs
@@ -0,0 +1,7 @@
+/* a library that return back whatever you say to it */
+//create a function that returns a string you pass to it
+pub fn return_string(s: String) -> String {
+    //print the string you passed to it
+    println!("{}", s);
+    s
+}
